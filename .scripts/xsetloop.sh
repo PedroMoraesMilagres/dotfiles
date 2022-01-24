460e79cffb0198b5d2f2132a3dc709dcc523b883
PedroMoraesMilagres <35903097+PedroMoraesMilagres@users.noreply.github.com>
2022-01-24 16:48:57 -0300

    add some features to the bar

--- .scripts/xsetloop.sh
+++ .scripts/xsetloop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while : ; do
-    xsetroot -name "  wlp5s0: $(cat /sys/class/net/wlp5s0/operstate) |  $(date '+%d/%m/%Y') |  $(date +%H:%M) "
-    sleep 1m
+  xsetroot -name "$(sh ~/.scripts/info.sh)"
+  sleep 1s
 done
