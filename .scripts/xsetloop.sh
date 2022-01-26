b6f3bca8712b293065651482d7dc31d678c13d99
PedroMoraesMilagres <35903097+PedroMoraesMilagres@users.noreply.github.com>
2022-01-25 21:47:17 -0300

    new scripts fow dwmbar

--- .scripts/xsetloop.sh
+++ .scripts/xsetloop.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-while : ; do
-  xsetroot -name "$(sh ~/.scripts/info.sh)"
-  sleep 1s
-done
+while true; do
+  CPU=$(~/.scripts/dwmcpu)
+  MEM=$(~/.scripts/dwmmemory)
+	NET=$(~/.scripts/dwmnet)
+	TEMP=$(~/.scripts/dwmtemp)
+	HDD=$(~/.scripts/dwmdisk)
+	TIME=$(~/.scripts/dwmclock)
+	xsetroot -name "|$CPU | $TEMP | $MEM | $NET | $HDD | $TIME"
+
+	sleep 5s
+	
+done &
+
