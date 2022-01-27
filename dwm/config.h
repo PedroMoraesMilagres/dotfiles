cb525996d073c8dd2ff8ed898b8603229609482f
PedroMoraesMilagres <35903097+PedroMoraesMilagres@users.noreply.github.com>
2022-01-26 23:03:43 -0300

    added new bar

--- dwm/config.h
+++ dwm/config.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SymbolsNerdFont:size=13", "UbuntuMono:size=13" };
+static const char *fonts[]          = { "SymbolsNerdFont:size=13.5", "UbuntuMono:size=13.5", "Font Awesome 5:size=10" };
 static const char dmenufont[]       = "UbuntuMono:size=13";
-static const char col_gray1[]       = "#c72020"; /* 222222 */
+static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#AF2934";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", ""};
+static const char *tags[] = { "", "", "", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -30,8 +30,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "discord",  NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "emacs",    NULL,       NULL,       1 << 4,       0,           -1 },
-	{ "emacs-27.2",  NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "Emacs",    NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
